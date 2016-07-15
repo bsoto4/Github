3e5167a418f41375929504ae1165830248c000ec
bsoto <bryam.soto@unmsm.edu.pe>
2016-07-15 12:13:25 -0500

    malogrando el codigo

--- app/src/main/java/doapps/github/MainActivity.java
+++ app/src/main/java/doapps/github/MainActivity.java
@@ -9,7 +9,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*CODIGO CHEVERE*/
+        /*
+        *
+        *
+        *
+        * codigo malo
+        *
+        *
+        *
+        * */
     }
 }
 
